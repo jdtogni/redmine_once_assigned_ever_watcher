9b3bd7e9ebe6830ab74cf2a973a53cf64bd36120
Daniel Togni <dtogni@trialpay.com>
2014-06-05 18:03:58 -0400

    Adding PM Owner and Eng Onwer as watchers

--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -3,7 +3,14 @@ module OnceAssignedEverWatcher
   
     def controller_issues_edit_before_save (context={})
       if context[:issue] && context[:issue].id != nil                      
-        issue = Issue.find_by_id(context[:issue].id)      
+        issue = Issue.find_by_id(context[:issue].id)
+        issue.custom_field_values.each do |value|
+          if value.custom_field.name = 'PM Owner' || value.custom_field.name = 'Eng Owner'
+            Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
+                                                                                  context[:issue].id, 
+                                                                                  value.value)
+          end
+        end       
         if issue.assigned_to_id != nil
           Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
                                                                                 context[:issue].id, 
